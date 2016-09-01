9f8266d9075ec355640820d5b541662d2b280326
Adriano Lima Candido <jink_adriano@hotmail.com>
2016-09-01 19:03:04 -0300

    Mudança nos Nomes
    Mudança dos nomes

--- Bird Point/Source/BirdPoint/src/birdpoint/telas/MenuPrincipal.java
+++ Bird Point/Source/BirdPoint/src/birdpoint/telas/MenuPrincipal.java
@@ -9,12 +9,12 @@ package birdpoint.telas;
  *
  * @author Marcelio
  */
-public class Rh extends javax.swing.JFrame {
+public class MenuPrincipal extends javax.swing.JFrame {
 
     /**
      * Creates new form TelaRh
      */
-    public Rh() {
+    public MenuPrincipal() {
         initComponents();
     }
 
@@ -107,14 +107,22 @@ public class Rh extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Rh.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MenuPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Rh.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MenuPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Rh.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MenuPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Rh.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MenuPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
@@ -127,7 +135,7 @@ public class Rh extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Rh().setVisible(true);
+                new MenuPrincipal().setVisible(true);
             }
         });
     }
