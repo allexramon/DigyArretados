ba96b85d26e372b26175a76fc21a35610153a5ed
Risoaldo <noobregarisoaldo@gmail.com>
2016-09-02 09:45:05 -0300

    juzenio alterou, semestre TableModel,SemestreDAO
    Foram criadas as classas  TableModel,SemestreDAO

--- Bird Point/Source/BirdPoint/src/birdpoint/semestre/SemestreTableModel.java
+++ Bird Point/Source/BirdPoint/src/birdpoint/semestre/SemestreTableModel.java
@@ -5,10 +5,69 @@
  */
 package birdpoint.semestre;
 
+import birdpoint.professor.Professor;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
 /**
  *
  * @author Adriano Lima
  */
-public class SemestreTableModel {
+public class SemestreTableModel extends AbstractTableModel{
+      private List<Semestre> semestres = new ArrayList<>();
+    private String[] colunas = {"nome semestre", "curso","id semestre","nome semestre"};
+
+    public SemestreTableModel(List<Semestre> semestre) {
+        this.semestres = semestre;
+    }
+     @Override
+    public int getRowCount() {
+        return semestres.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colunas.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex)  {
+        Semestre  semestre = semestres.get(rowIndex);
+        switch (columnIndex) {
+            case 0:
+                return semestre.getNomeSemestre();
+            
+              
+            case 1:
+                return semestre.getCurso();
+            case 2:
+                return semestre.getIdSemestre();
+            case 3:
+                return semestre.getNomeSemestre();
+            
+        }
+        return null;
+    }
+
+    public String getColumnName(int index) {
+        switch (index) {
+            case 0:
+                return colunas[0];
+            case 1:
+                return colunas[1];
+            case 2:
+                return colunas[2];
+            case 3:
+                return colunas[3];
+            
+                
+            
+
+
+        }
+        return null;
+    }
+
     
 }
