b8c7967f0d2805501f08bc39976cf0f52715f8c5
Risoaldo <noobregarisoaldo@gmail.com>
2016-09-02 08:35:18 -0300

    Curso criado
    Criei o CursoTableModel e o CursoDAO, sem erros

--- Bird Point/Source/BirdPoint/src/birdpoint/curso/CursoTableModel.java
+++ Bird Point/Source/BirdPoint/src/birdpoint/curso/CursoTableModel.java
@@ -5,10 +5,55 @@
  */
 package birdpoint.curso;
 
+import birdpoint.cidade.Cidade;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
 /**
  *
  * @author Adriano Lima
  */
-public class CursoTableModel {
+public class CursoTableModel extends AbstractTableModel{
+    
+    private List<Curso> cursos = new ArrayList<>();
+    private String[] colunas = {"Id Curso", "Nome Curso"};
+
+    public CursoTableModel(List<Curso> curso) {
+        this.cursos = curso;
+    }
+
+    @Override
+    public int getRowCount() {
+        return cursos.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colunas.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Curso curso = cursos.get(rowIndex);
+        switch (columnIndex) {
+            case 0:
+                return curso.getNomeCurso();
+            case 1:
+                return curso.getIdCurso();
+        }
+        return null;
+    }
+
+    public String getColumnName(int index) {
+        switch (index) {
+            case 0:
+                return colunas[0];
+            case 1:
+                return colunas[1];
+        }
+        return null;
+    }
+
     
 }
