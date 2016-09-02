a944876b1ff08db3504cf512298f27d8f0c9664e
Risoaldo <noobregarisoaldo@gmail.com>
2016-09-02 16:17:29 -0300

    Usuario concluido
    Foi criado o TableModel, UsuarioDAO e todos os atributos de acordo com o
    documento.

---
+++ Bird Point/Source/BirdPoint/src/birdpoint/usuario/UsuarioDAO.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package birdpoint.usuario;
+
+import birdpoint.util.GenericDAO;
+
+/**
+ *
+ * @author Nóbrega
+ */
+public class UsuarioDAO extends GenericDAO<Usuario>{
+
+    public UsuarioDAO() {
+        super(Usuario.class);
+    }
+    
+}
--- Bird Point/Source/BirdPoint/src/birdpoint/usuario/UsuarioTableModel.java
+++ Bird Point/Source/BirdPoint/src/birdpoint/usuario/UsuarioTableModel.java
@@ -5,10 +5,68 @@
  */
 package birdpoint.usuario;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
 /**
  *
  * @author Adriano Lima
  */
-public class UsuarioTableModel {
+public class UsuarioTableModel extends AbstractTableModel {
+    
+    private List<Usuario> usuarios = new ArrayList<>();
+    private String[] colunas = {"Nome", "Login", "Senha","Tipo Acesso","ID"};
+
+    public UsuarioTableModel(List<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    @Override
+    public int getRowCount() {
+        return usuarios.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colunas.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Usuario usuario = usuarios.get(rowIndex);
+        switch (columnIndex) {
+            case 0:
+                return usuario.getNomeUsuario();
+            case 1:
+                return usuario.getLoginUsuario();
+            case 2:
+                return usuario.getSenhaUsuario();
+            case 3:
+                return usuario.getTipoDeAcedssoUsuario();
+            case 4:
+                return usuario.getIdUsuario();
+
+        }
+        return null;
+    }
+
+    public String getColumnName(int index) {
+        switch (index) {
+            case 0:
+                return colunas[0];
+            case 1:
+                return colunas[1];
+            case 2:
+                return colunas[2];
+            case 3:
+                return colunas[3];
+            case 4:
+                return colunas[4];
+ 
+        }
+        return null;
+    }
+
     
 }
